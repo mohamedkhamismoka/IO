a3068e06c1766b80f543cd2649d65dc308cb7084
mohameddatif <99369751+mohameddatif@users.noreply.github.com>
2023-07-02 16:21:23 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
-$(document).ready(function() {
-    $(".navbar-toggler-icon").click(function() {
-        if ($(".Arrow").css("display") != "none") {
-            $(".Arrow").css("display", "none");
-        } else {
-            $(".Arrow").css("display", "block");
-        }
-        if ($(".links").css("display") != "none") {
-            $(".links").css("display", "none");
-        } else {
-            $(".links").css("display", "block");
-        }
-
-
-    })
-
-
-})
+$(document).ready(function(){$(".navbar-toggler-icon").click(function(){"none"!=$(".Arrow").css("display")?$(".Arrow").css("display","none"):$(".Arrow").css("display","block"),"none"!=$(".links").css("display")?$(".links").css("display","none"):$(".links").css("display","block")})});
